09d11a55ed4aa930dea31376727a5559869fd6ce
张泽 <zhangze.1996@bytedance.com>
2019-06-24 00:03:23 +0800

    fix: publicPath

--- scripts/webpack.config.prod.js
+++ scripts/webpack.config.prod.js
@@ -1,6 +1,6 @@
 const commonConfig = require("./webpack.config");
 
 commonConfig.mode = "production";
-commonConfig.output.publicPath = "/mini-two.js-example/public";
+commonConfig.output.publicPath = "/mini-two.js-example/public/";
 
 module.exports = commonConfig;
